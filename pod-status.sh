979c91739f99061d011ac27355cc7640cdbeead8
Administrator <admin@example.com>
2018-10-04 12:18:19 +0000

    Update pod-status.sh

--- pod-status.sh
+++ pod-status.sh
@@ -12,6 +12,6 @@ if [[ $var =~ $var2 ]];
 then
  echo "Pod is deployed successfully"
 else
-    source rolling-bach.sh; rolling-back-script $DEPLOYMENT_NAME $IMAGE_NAME 
+    source rolling-bach.sh; rolling_back_script $DEPLOYMENT_NAME $IMAGE_NAME 
 fi
 }
