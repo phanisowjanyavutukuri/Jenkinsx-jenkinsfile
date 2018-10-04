21cd9c40f3e5298f8e974f1266a668d4fdc30772
Administrator <admin@example.com>
2018-10-04 13:55:50 +0000

    Update pod-status.sh

--- pod-status.sh
+++ pod-status.sh
@@ -1,7 +1,7 @@
 function pod_status()
 {
 local DEPLOYMENT_NAME=${1:?Provide the DEPLOYMENT-NAME}
-local IMAGE_NAME=${2:?Provide the IMAGE_NAME}
+
 var=$(kubectl rollout status deploy/$DEPLOYMENT_NAME)
 echo $var
 var2="successfully"
@@ -9,6 +9,6 @@ if [[ $var =~ $var2 ]]
 then
  echo "Pod is deployed successfully"
 else
-    source rolling-back.sh; rolling_back_script $DEPLOYMENT_NAME $IMAGE_NAME 
+    kubectl rollout undo deployment/$DEPLOYMENT_NAME --revision=1
 fi
 }
