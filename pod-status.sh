b147720793cddbe3f3a25dca718b18744d0ef9c0
Administrator <admin@example.com>
2018-10-04 13:45:58 +0000

    Update pod-status.sh

--- pod-status.sh
+++ pod-status.sh
@@ -4,7 +4,7 @@ local DEPLOYMENT_NAME=${1:?Provide the DEPLOYMENT-NAME}
 local IMAGE_NAME=${2:?Provide the IMAGE_NAME}
 var=$(kubectl rollout status deploy/$DEPLOYMENT_NAME)
 echo $var
-var2="successfully"`
+var2="successfully"
 if [[ $var =~ $var2 ]]
 then
  echo "Pod is deployed successfully"
