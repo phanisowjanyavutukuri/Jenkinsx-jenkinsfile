94048ab80a60a65628f8c10503f9a3d6523d5362
Administrator <admin@example.com>
2018-10-05 05:07:06 +0000

    Update pod-status.sh

--- pod-status.sh
+++ pod-status.sh
@@ -2,13 +2,13 @@ function pod_status()
 {
 local DEPLOYMENT_NAME=${1:?Provide the DEPLOYMENT-NAME}
 
-var=$(kubectl rollout status deploy/$DEPLOYMENT_NAME)
+var=$(kubectl rollout status deploy/$DEPLOYMENT_NAME -n cloudwms-dev)
 echo $var
 var2="successfully"
 if [[ $var =~ $var2 ]]
 then
  echo "Pod is deployed successfully"
 else
-    kubectl rollout undo deployment/$DEPLOYMENT_NAME --to-revision=1
+    kubectl rollout undo deployment/$DEPLOYMENT_NAME --to-revision=1 -n cloudwms-dev
 fi
 }
